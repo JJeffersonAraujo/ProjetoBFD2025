49251a7f7a82e012b65145bf16fd465dd39a190a
Jefferson Araujo <jeffersonaraujo.pe@gmail.com>
2025-08-21 18:21:02 -0300

    cálculo de média e aprovação de aluno

---
+++ index.js
@@ -0,0 +1,74 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function pergunta(texto) {
+  return new Promise((resolve) => {
+    rl.question(texto, (resposta) => resolve(resposta));
+  });
+}
+
+async function notas () {
+  let nota1 = Number(await pergunta("Digite sua primeira nota: "));
+
+  while (nota1 < 0 || nota1 > 10) {
+    console.log("Nota inválida. Digite novamente: ");
+    nota1 = Number(await pergunta("Digite sua primeira nota: "));
+  }
+
+  console.log("Nota válida:", nota1);
+
+  let nota2 = Number(await pergunta("Digite sua segunda nota: "));
+
+  while (nota2 < 0 || nota2 > 10) {
+    console.log("Nota inválida. Digite novamente: ");
+    nota2 = Number(await pergunta("Digite sua segunda nota: "));
+  }
+
+  console.log("Nota válida:", nota2);
+
+
+    let nota3 = Number(await pergunta("Digite sua terceira nota: "));
+
+  while (nota3 < 0 || nota3 > 10) {
+    console.log("Nota inválida. Digite novamente: ");
+    nota3 = Number(await pergunta("Digite sua terceira nota: "));
+  }
+
+  console.log("Nota válida:", nota3);
+
+
+    let nota4 = Number(await pergunta("Digite sua quarta nota: "));
+
+  while (nota4 < 0 || nota4 > 10) {
+    console.log("Nota inválida. Digite novamente: ");
+    nota4 = Number(await pergunta("Digite sua quarta nota: "));
+  }
+
+  console.log("Nota válida:", nota4);
+
+
+    let nota5 = Number(await pergunta("Digite sua quinta nota: "));
+
+  while (nota5 < 0 || nota5 > 10) {
+    console.log("Nota inválida. Digite novamente: ");
+    nota5 = Number(await pergunta("Digite sua quinta nota: "));
+  }
+
+  console.log("Nota válida:", nota5);
+
+
+let media = (nota1 + nota2 + nota3 + nota4 + nota5) /5;
+
+if (media >= 7 && media <= 10) {
+console.log ('Aprovado!');
+} else if (media >= 0) {
+console.log ('Reprovado!');
+} else {
+console.log ('Média inválida.');
+}
+    rl.close();
+}
+notas();
